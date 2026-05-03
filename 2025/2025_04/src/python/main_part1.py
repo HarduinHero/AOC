030b79f7ba9d7cd877341eb54d7038166f2d0b94
Harduin_Hero <harduinero@gmail.com>
2026-05-03 14:24:54 +0200

    2025-04

--- 2025/2025_04/src/python/main_part1.py
+++ 2025/2025_04/src/python/main_part1.py
@@ -7,13 +7,36 @@ def get_filecontent(filename:str) -> list[str] :
         output = file.readlines()
     return output
 
+def print_grid(grid) :
+    for l in grid :
+        print(''.join(l))
+
+def get_adjacent(grid:list[list[str]], y:int, x:int) -> list[str]:
+    y_max, x_max = len(grid)-1, len(grid[0])-1
+    coord_to_check = [(y-1,x-1), (y-1,x), (y-1,x+1), (y,x-1), (y,x+1), (y+1,x-1), (y+1,x), (y+1,x+1)]
+    coord_to_check = [coord for coord in coord_to_check if coord[0]>=0 and coord[1]>=0 and coord[0]<=y_max and coord[1]<=x_max]
+    return [grid[coord[0]][coord[1]] for coord in coord_to_check]
+
 def main_p1(target:str) -> int :
-    input_data = get_filecontent(target)
-    return 0
+    grid = [[c for c in line.replace('\n','')] for line in get_filecontent(target)]
+    debug_grid = [[c for c in l] for l in grid]
+    output = 0
+    for y in range(len(grid)) :
+        for x in range(len(grid[0])) :
+            if grid[y][x] != '@' :
+                continue
+            adj = get_adjacent(grid, y, x)
+            #print(f"({y}, {x}) - {adj}")
+            if adj.count("@") < 4 :
+                output += 1
+                debug_grid[y][x] = "X"
+
+    print_grid(debug_grid)
+    return output
     
 if __name__ == "__main__" :
 
-    #target = "src/input_ex1.txt"
+    target = "src/input_ex1.txt"
     target = "src/input.txt"
 
     out = main_p1(target)
--- 2025/2025_04/src/python/main_part2.py
+++ 2025/2025_04/src/python/main_part2.py
@@ -2,15 +2,39 @@
 # Advent of Code 2025 (https://adventofcode.com/)
 # Puzzle 04
 
-from main_part1 import get_filecontent
+from main_part1 import get_filecontent, print_grid, get_adjacent
 
 def main_p2(target:str) -> int:
-    input_data = get_filecontent(target)
-    return 0
+    output = 0
+    current_grid = [[c for c in line.replace('\n','')] for line in get_filecontent(target)]
+    print("Initial state:")
+    print_grid(current_grid)
+
+    while True :
+        next_grid = [[c for c in l] for l in current_grid]
+        removable_rolls = 0
+        
+        for y in range(len(current_grid)) :
+            for x in range(len(current_grid[0])) :
+                if current_grid[y][x] != '@' :
+                    continue
+                adj = get_adjacent(current_grid, y, x)
+                if adj.count("@") < 5 :
+                    removable_rolls += 1
+                    next_grid[y][x] = "X"
+        
+        output += removable_rolls
+        if removable_rolls == 0 :
+            break
+        print(f"Remove {removable_rolls} rolls of paper:")
+        print_grid(next_grid)
+        current_grid = [['@' if c=='@' else '.' for c in l] for l in next_grid]
+
+    return output
 
 if __name__ == "__main__" :
 
-    #target = "src/input_ex1.txt"
+    target = "src/input_ex1.txt"
     target = "src/input.txt"
 
     out = main_p2(target)
